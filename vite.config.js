c2bbfe4c3603de0f23e9a7c530b1ccdd96cb5337
Xenofon <75397560+XenofonGk@users.noreply.github.com>
2026-07-06 12:13:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: ".", // Drops production files into your root folder instead of /dist
-    emptyOutDir: false, // Prevents Vite from deleting your source code
-  },
-});
+  // Vite will now cleanly build your site into a folder named "dist"
+})
